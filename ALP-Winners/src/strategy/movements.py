096224f8f6223055e4962c5558b7c15777cbcc8f
André <andredornelas23@gmail.com>
2022-10-10 21:48:14 -0300

    Suport on goalkeeper constant changes

--- ALP-Winners/src/strategy/movements.py
+++ ALP-Winners/src/strategy/movements.py
@@ -79,18 +79,18 @@ def goalkeep(rb, vb, rr, rg):
     xGoal = rg[0] 
 
     #projeta a velocidade da bola 
-    ytarget = projectLine(rb, vb, xGoal+0.05)
-    if ((vb[0]) < -0.03): #and  ((rb[0]) > .15) and np.abs(ytarget) < 0.2:
-        #verificar se a projeção está no gol
-        ytarget = sat(ytarget, 0.2)
+    # ytarget = projectLine(rb, vb, xGoal+0.05)
+    ytarget = projectLine(rb, vb, xGoal)
+    if ((vb[0]) < -0.1): #and  ((rb[0]) > .15) and np.abs(ytarget) < 0.2: (verifica se a projeção está no gol)
+        ytarget = sat(ytarget, 0.20)
         angle = np.pi/2 if rr[1] < ytarget else -np.pi/2
         return (xGoal, ytarget, angle)
 
     #Se não, acompanha o y
-    ytarget = sat(rb[1], 0.2 + 0.27 * (rb[0] < -0.6 and abs(rb[1]) < 0.37))
+    # ytarget = sat(rb[1], 0.2 + 0.27 * (rb[0] < -0.6 and abs(rb[1]) < 0.37)) #(permite que o goleiro avance na área do gol)
     angle = np.pi/2 if rr[1] < ytarget else -np.pi/2
     return np.array([xGoal, ytarget, angle])
- 
+
 def blockBallElipse(rb, vb, rr, rm, a, b):
     e = np.array([1/a, 1/b])
     spin = 0
