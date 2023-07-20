7c2d92e65b46ee31eecb71a381a4c0127ec68e2f
André <andredornelas23@gmail.com>
2023-07-20 17:57:04 -0300

    remove controle antigo

--- ALP-Winners/src/control/__init__.py
+++ ALP-Winners/src/control/__init__.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 from tools import speeds2motors, deadzone, sat
-from control.motorControl import MotorControl
 import numpy as np
 import time
 
@@ -10,12 +9,6 @@ class Control(ABC):
         ABC.__init__(self)
 
         self.world = world
-        self.motor_vr_control = MotorControl(3, 0.2 , 0.05, 32)
-        self.motor_vl_control = MotorControl(3, 0.2, 0.05, 32)
-        # self.motor_vr_control = MotorControl(1, 1, 3, 32)
-        # self.motor_vl_control = MotorControl(1, 1, 3, 32)
-
-        self.last_w = 0
 
     @abstractmethod
     def output(self, robot):
@@ -42,43 +35,23 @@ class Control(ABC):
             return (0, 0)
 
         v, w = self.output(robot)
-        # v = 0.1 * np.sign(np.sin(2*np.pi*0.5*time.time()))
-        # w = 0
         robot.lastControlLinVel = v
         
         vr, vl = speeds2motors(v, w)
-        vision_w = self.last_w + sat(robot.angvel-self.last_w, 0.1)
-        vision_vr, vision_vl = speeds2motors(robot.v_signed, vision_w)
 
-        ur = self.motor_vr_control.actuate(vr, vr - vision_vr)
-        ul = self.motor_vl_control.actuate(vl, vl - vision_vl)
+        vr = int(deadzone(sat(vr, 255), 32, -32))
+        vl = int(deadzone(sat(vl, 255), 32, -32))
 
-        if robot.id == 0:
-            pass
-            # print(
-            #     f"ur: {ur}\ter: {vr - vision_vr}, vr: {vr}, vision_vr: {vision_vr}")
-            # print(
-            #     f"ul: {ul}\tel: {vl - vision_vl}, vl: {vl}, vision_vl: {vision_vl}")
-            # print(
-            #     f"ur: {ur}\tev: {v - robot.v_signed}, v: {v}, vision_v: {robot.v_signed}")
-            # print(
-            #     f"ul: {ul}\tew: {w - vision_w}, w: {w}, vision_w: {vision_w}")
+        # print(
+        #     f"ur: {ur}\ter: {vr - vision_vr}, vr: {vr}, vision_vr: {vision_vr}")
+        # print(
+        #     f"ul: {ul}\tel: {vl - vision_vl}, vl: {vl}, vision_vl: {vision_vl}")
+        # print(
+        #     f"ur: {ur}\tev: {v - robot.v_signed}, v: {v}, vision_v: {robot.v_signed}")
+        # print(
+        #     f"ul: {ul}\tew: {w - vision_w}, w: {w}, vision_w: {vision_w}")
+        
+        return vr, vl
 
-        # vr, vl = speeds2motors(uv, uw)
 
-        self.last_w = vision_w
-        return ur, ul
 
-        # vr, vl = speeds2motors(v, self.world.field.side * w)
-
-        # # return (vr, vl)
-
-        # vision_vr, vision_vl = speeds2motors(robot.velmod, robot.angvel)
-
-        # ur = self.motor_r_control.actuate(vr - vision_vr)
-        # ul = self.motor_l_control.actuate(vl - vision_vl)
-
-        # if robot.id == 0:
-        #     print(f"ur: {ur}, e: {vr-vision_vr}, vr: {vr}, vision_vr: {vision_vr}")
-
-        # return (int(ur), int(ul))
