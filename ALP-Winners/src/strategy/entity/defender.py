d84e7570f3aed571452db0fb2cea8d59cb08a664
André <andredornelas23@gmail.com>
2023-07-30 15:46:56 -0300

    defender volta a usar defendercontrol

--- ALP-Winners/src/strategy/entity/defender.py
+++ ALP-Winners/src/strategy/entity/defender.py
@@ -17,8 +17,8 @@ class Defender(Entity):
     def __init__(self, world, robot, side=1):
         super().__init__(world, robot)
 
-        # self._control = DefenderControl(self.world)
-        self._control = UFC_Simple(self.world)
+        self._control = DefenderControl(self.world)
+        # self._control = UFC_Simple(self.world)
     @property
     def control(self):
         return self._control
